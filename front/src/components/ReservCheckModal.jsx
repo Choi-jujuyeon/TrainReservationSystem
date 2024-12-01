76087664d7e6f240fa90e3d40e2acdc3c9ae7c12
Choi-jujuyeon <chlwndus1216@naver.com>
2024-12-02 07:55:55 +0900

    feat: implement discount recommendation feature based on user answers

--- front/src/components/ReservCheckModal.jsx
+++ front/src/components/ReservCheckModal.jsx
@@ -27,26 +27,22 @@ const ReservCheckModal = ({ onClose, links, title, message }) => {
 };
 
 export default ReservCheckModal;
+
 const CustomLink = styled(Link)`
     text-decoration: none;
 `;
+
 const Overlay = styled.div`
     position: fixed;
-    /* top: 0; */
-    /* left: 0; */
-    /* right: 0; */
-    /* bottom: 0; */
     background-color: rgba(0, 0, 0, 0.5);
     display: flex;
     justify-content: center;
     align-items: center;
     width: 375px;
-    /* width: 100%; */
-    /* height: 100%; */
+
     height: 812px;
     z-index: 9999;
     border-radius: 32px;
-    margin: auto;
 `;
 
 const ModalContainer = styled.div`
